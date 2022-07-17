7089d90600775e7dbdcca52c1583546126868b96
KH0RE <cet.quinaluisa@yavirac.edu.ec>
2022-07-16 22:06:25 -0500

    New proyect

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@100&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light ">
+        <a class="navbar-brand"  href="#">Nombre de la Empresa</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#plan">Crecer</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">Quienes Somos</a>
+              </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+      <br>
+      {{ $slot }}
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>Inversion</title>
+
+
+</head>
+<body>
+    <x-menu>
+            <div class="container-fluid cuadro">
+                <div class="row">
+                    <div class="col-md-6 col-sm-12 "  >
+                        <h2 class="titulo" >Ya es hora de empezar a construir tus metas</h2>
+
+                        <p class="texto">
+                            Invierte con nosotros y cumple todos tus sueños
+                        </p>
+                    </div>
+
+                    <div class="col-md-6 col-sm-12 ">
+                      <img src="{{ URL('img/foto.png') }}" style="width: 80%; "  alt="">
+
+                    </div>
+                </div>
+            </div>
+            <section id="plan">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="text-center crece" >Con nosotros tu dinero CRECE!!!</h2>
+                        <hr>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="container" id="cards">
+                <div class="row">
+                    <div class="col-md-4 col-sm-12">
+                        <div class="card">
+                            <img class="money" src="{{ URL('img/botones/money.png') }}" alt="">
+                            <h3 class="text-center ti1"> Con un pequeño Monto </h3>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12">
+                        <div class="card">
+                            <img class="money" src="{{ URL('img/botones/reloj.png') }}" alt="">
+                            <h3 class="text-center ti1"> Tu dinero se Multiplicara </h3>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-12">
+                        <div class="card">
+                            <img class="money" src="{{ URL('img/botones/bolsa.png') }}" alt="">
+                            <h3 class="text-center ti1"> Retira tus Ganancias </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+            <section id="about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center crece" >Quienes Somos</h2>
+                            <hr>
+                            <br>
+                            <br>
+                        </div>
+                    </div>
+                </div>
+                <div class="container-fluid" id="info">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2 class="text-center empresa" >Nombre de la Empresa</h2>
+                            <br>
+                            <br>
+                        </div>
+
+                        <div class="col-md-6 col-sm-12">
+                            <p>
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis quaerat pariatur commodi expedita aliquam deserunt accusantium quasi deleniti sapiente quia similique ex nostrum suscipit facilis, odit possimus veniam iusto. Illo?
+                            </p>
+                        </div>
+                        <div class="col-md-6 col-sm-12">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, dignissimos provident corrupti rerum inventore eum perspiciatis alias ea sunt accusantium suscipit neque laborum assumenda veniam? Nulla at exercitationem dicta nihil?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+    </x-menu>
+</body>
+</html>
